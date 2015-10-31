0d8929243b5122e95da1004efbf99fc3e0bd7267
Joshua Granick <jgranick@users.noreply.github.com>
2015-10-31 09:24:48 -0700

    Embed all dependencies

--- Source/Main.hx
+++ Source/Main.hx
@@ -3,6 +3,8 @@ package;
 
 import haxe.io.Path;
 import haxe.macro.Compiler;
+import lime.project.Haxelib;
+import lime.project.Platform;
 import lime.system.BackgroundWorker;
 import lime.tools.helpers.PathHelper;
 import lime.tools.helpers.PlatformHelper;
@@ -19,12 +21,6 @@ import task.TaskManager;
 
 
 class Main extends Display {
-	
-	
-	private static var TASK_INSTALL_HAXE = "installHaxe";
-	private static var TASK_INSTALL_LIME = "installLime";
-	private static var TASK_INSTALL_OPENFL = "installOpenFL";
-	private static var TASK_SETUP_OPENFL = "setupOpenFL";
 	
 	
 	public function new () {
@@ -46,57 +42,62 @@ class Main extends Display {
 		
 		StatusText.text = "";
 		
-		//var tempFile = PathHelper.getTemporaryFile ();
-		//var output = ProcessHelper.runProcess ("", "cmd", [ "/C", "haxelib", "version" ], true, true, true);
-		//Sys.println (StringTools.trim (output));
-		//if (FileSystem.exists (tempFile)) {
-			//
-			//Sys.println (tempFile);
-			//
-		//}
-		
 	}
 	
 	
 	private function installHaxe ():Void {
 		
-		StatusText.text = "Running Haxe Installer...";
+		StatusText.text = "Running Haxe Installer (" + Compiler.getDefine ("HAXE_VERSION") + ")...";
 		
 		Actuate.timer (1.2).onComplete (function () {
 			
 			var path = PathHelper.getTemporaryDirectory ();
 			PathHelper.mkdir (path);
-			path = PathHelper.combine (path, "haxe-installer.exe");
+			
+			switch (PlatformHelper.hostPlatform) {
+				
+				case WINDOWS: path = PathHelper.combine (path, "haxe-" + Compiler.getDefine ("HAXE_VERSION") + "-win.exe");
+				case MAC: path = PathHelper.combine (path, "haxe-" + Compiler.getDefine ("HAXE_VERSION") + "-osx-installer.pkg");
+				default:
+				
+			}
+			
 			File.saveBytes (path, Assets.getBytes ("haxe"));
 			
 			switch (PlatformHelper.hostPlatform) {
 				
-				case WINDOWS: ProcessHelper.runProcess ("", "cmd", [ "/c", path ], true, true, true);
-				case MAC: ProcessHelper.runProcess ("", "open", [ path ], true, true, true);
-				default: ProcessHelper.runProcess ("", "xdg-open", [ path ], true, true, true);
+				case WINDOWS: runProcess (path);
+				case MAC: runProcess ("open", [ path ]);
+				default: runProcess ("xdg-open", [ path ]);
 				
 			}
 			
 			PathHelper.removeDirectory (Path.directory (path));
-			TaskManager.completeTask (TASK_INSTALL_HAXE);
+			TaskManager.completeTask ("installHaxe");
 			
 		});
 		
 	}
 	
 	
-	private function installLime ():Void {
+	private function installHaxelib (name:String, key:String, segmentCount:Int = 1, version:String = "1.0.0"):Void {
 		
-		StatusText.text = "Installing Lime...";
+		StatusText.text = "Installing " + name + " (" + version + ")...";
 		
 		var path = PathHelper.getTemporaryFile (".zip");
 		var output = File.write (path);
 		
-		var limeSegmentCount = Std.parseInt (Compiler.getDefine ("LIME_SEGMENT_COUNT"));
-		
-		for (i in 0...limeSegmentCount) {
+		if (segmentCount < 2) {
 			
-			output.write (Assets.getBytes ("lime_segment" + i));
+			output.write (Assets.getBytes (key));
+			
+		} else {
+			
+			for (i in 0...segmentCount) {
+				
+				output.write (Assets.getBytes (key + ".segment" + i));
+				
+			}
 			
 		}
 		
@@ -105,64 +106,77 @@ class Main extends Display {
 		var worker = new BackgroundWorker ();
 		worker.doWork.add (function (_) {
 			
-			ProcessHelper.runProcess ("", "haxelib", [ "local", path ], true, true, true);
+			runProcess ("haxelib", [ "local", path ]);
 			worker.sendComplete ();
 			
 		});
-			
+		
 		worker.onComplete.add (function (_) {
+			
 			try {
 				
 				FileSystem.deleteFile (path);
 				
 			} catch (e:Dynamic) { }
 			
-			TaskManager.completeTask (TASK_INSTALL_LIME);
+			TaskManager.completeTask ("install" + name);
 			
 		});
+		
 		worker.run ();
 		
 	}
 	
 	
-	private function installOpenFL ():Void {
+	private function runProcess (command:String, args:Array<String> = null):String {
 		
-		StatusText.text = "Installing OpenFL...";
+		if (args == null) args = [];
 		
-		var path = PathHelper.getTemporaryFile (".zip");
-		File.saveBytes (path, Assets.getBytes ("openfl"));
-		
-		var worker = new BackgroundWorker ();
-		worker.doWork.add (function (_) {
+		return switch (PlatformHelper.hostPlatform) {
 			
-			ProcessHelper.runProcess ("", "haxelib", [ "local", path ], true, true, true);
-			worker.sendComplete ();
+			case WINDOWS: ProcessHelper.runProcess ("", "cmd", [ "/c", command ].concat (args), true, true, true);
+			default: ProcessHelper.runProcess ("", command, args, true, true, true);
 			
-		});
-		worker.onComplete.add (function (_) {
-			
-			try {
-				
-				FileSystem.deleteFile (path);
-				
-			} catch (e:Dynamic) { }
-			
-			TaskManager.completeTask (TASK_INSTALL_OPENFL);
-			
-		});
-		worker.run ();
+		}
 		
 	}
 	
 	
 	private function setupOpenFL ():Void {
 		
-		StatusText.text = "Setting Up OpenFL...";
+		StatusText.text = "Installing \"openfl\" Command...";
 		
 		var worker = new BackgroundWorker ();
 		worker.doWork.add (function (_) {
 			
-			ProcessHelper.runProcess ("", "haxelib", [ "run", "openfl", "setup", "-y" ], true, true, true);
+			var haxePath = Sys.getEnv ("HAXEPATH");
+			
+			if (PlatformHelper.hostPlatform == Platform.WINDOWS) {
+				
+				if (haxePath == null || haxePath == "") {
+					
+					haxePath = "C:\\HaxeToolkit\\haxe\\";
+					
+				}
+				
+				try { File.copy (PathHelper.getHaxelib (new Haxelib ("lime")) + "\\templates\\\\bin\\lime.exe", haxePath + "\\lime.exe"); } catch (e:Dynamic) {}
+				try { File.copy (PathHelper.getHaxelib (new Haxelib ("lime")) + "\\templates\\\\bin\\lime.sh", haxePath + "\\lime"); } catch (e:Dynamic) {}
+				try { File.copy (PathHelper.getHaxelib (new Haxelib ("openfl")) + "\\templates\\\\bin\\openfl.exe", haxePath + "\\openfl.exe"); } catch (e:Dynamic) {}
+				try { File.copy (PathHelper.getHaxelib (new Haxelib ("openfl")) + "\\templates\\\\bin\\openfl.sh", haxePath + "\\openfl"); } catch (e:Dynamic) {}
+				
+			} else {
+				
+				try {
+					
+					ProcessHelper.runCommand ("", "sudo", [ "cp", "-f", PathHelper.getHaxelib (new Haxelib ("lime")) + "/templates/bin/lime.sh", "/usr/local/bin/lime" ], false);
+					ProcessHelper.runCommand ("", "sudo", [ "chmod", "755", "/usr/local/bin/lime" ], false);
+					ProcessHelper.runCommand ("", "sudo", [ "cp", "-f", PathHelper.getHaxelib (new Haxelib ("openfl")) + "/templates/bin/openfl.sh", "/usr/local/bin/openfl" ], false);
+					ProcessHelper.runCommand ("", "sudo", [ "chmod", "755", "/usr/local/bin/openfl" ], false);
+					
+				} catch (e:Dynamic) {}
+				
+			}
+			
 			worker.sendComplete ();
 			
 		});
@@ -170,7 +184,7 @@ class Main extends Display {
 			
 			StatusText.text = "Done!";
 			
-			TaskManager.completeTask (TASK_SETUP_OPENFL);
+			TaskManager.completeTask ("setupOpenFL");
 			
 		});
 		worker.run ();
@@ -191,10 +205,26 @@ class Main extends Display {
 		Actuate.tween (InstallButton, 2, { alpha: 0 } );
 		Actuate.tween (StatusText, 2, { alpha: 1 } );
 		
-		TaskManager.addTask (new Task (TASK_INSTALL_HAXE, installHaxe), null, false);
-		TaskManager.addTask (new Task (TASK_INSTALL_LIME, installLime), [ TASK_INSTALL_HAXE ], false);
-		TaskManager.addTask (new Task (TASK_INSTALL_OPENFL, installOpenFL), [ TASK_INSTALL_LIME ], false);
-		TaskManager.addTask (new Task (TASK_SETUP_OPENFL, setupOpenFL), [ TASK_INSTALL_OPENFL ], false);
+		var installHaxeTask = new Task ("installHaxe", installHaxe);
+		TaskManager.addTask (installHaxeTask, null, false);
+		var previousTask = installHaxeTask;
+		
+		var names = [ "Lime", "OpenFL", "SWF", "HXCPP", "Actuate", "Box2D", "Layout", "OpenFL Samples", "Lime Samples" ];
+		var keys = [ "lime", "openfl", "swf", "hxcpp", "actuate", "box2d", "layout", "openfl-samples", "lime-samples" ];
+		var segments = [ Std.parseInt (Compiler.getDefine ("LIME_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("OPENFL_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("SWF_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("HXCPP_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("ACTUATE_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("BOX2D_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("LAYOUT_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("OPENFL_SAMPLES_SEGMENT_COUNT")), Std.parseInt (Compiler.getDefine ("LIME_SAMPLES_SEGMENT_COUNT")) ];
+		var versions = [ Compiler.getDefine ("LIME_VERSION"), Compiler.getDefine ("OPENFL_VERSION"), Compiler.getDefine ("SWF_VERSION"), Compiler.getDefine ("HXCPP_VERSION"), Compiler.getDefine ("ACTUATE_VERSION"), Compiler.getDefine ("BOX2D_VERSION"), Compiler.getDefine ("LAYOUT_VERSION"), Compiler.getDefine ("OPENFL_SAMPLES_VERSION"), Compiler.getDefine ("LIME_SAMPLES_VERSION") ];
+		
+		var task;
+		
+		for (i in 0...names.length) {
+			
+			task = new Task ("install" + names[i], installHaxelib, [ names[i], keys[i], segments[i], versions[i] ]);
+			TaskManager.addTask (task, [ previousTask ], false);
+			previousTask = task;
+			
+		}
+		
+		TaskManager.addTask (new Task ("setupOpenFL", setupOpenFL), [ previousTask ], false);
 		
 	}
 	
